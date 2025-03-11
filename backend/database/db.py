753b0a061d5be12a4ba597fd821701773c0e41e8
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2025-03-11 23:57:54 +0530

    databse connection

---
+++ backend/database/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from 
+# Database Configuration
+DB_USER = "postgres"
+DB_PASSWORD = "1234"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "esp32_data"
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Create the engine
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Function to connect to the database
+def init_db():
+    Base.metadata.create_all(bind=engine)
