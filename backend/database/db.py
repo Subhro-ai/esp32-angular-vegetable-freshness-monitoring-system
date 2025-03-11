9109a12d80130b373dfe1b4f151eff6c5f438294
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2025-03-11 23:59:41 +0530

    databse connection

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from 
 # Database Configuration
 DB_USER = "postgres"
 DB_PASSWORD = "1234"
